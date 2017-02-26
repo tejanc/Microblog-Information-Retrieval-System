34d4047cbccd05cf9f2d3b4a518755c98ced512c
Tejan Chinnakonda <tejan_rs@hotmail.com>
2017-02-26 18:47:35 -0500

    First working code for the Preprocessor
    It populates the stop word lists that is to be used in tokenization.

---
+++ src/IRSystem.java
@@ -0,0 +1,30 @@
+
+public class IRSystem{
+
+	public static void main(String[] args) {
+		
+		drawTitle();
+		                                                            
+		System.out.println();
+		
+		Preprocessing p = new Preprocessing(null);
+		
+		
+		//Preprocessing p = new Preprocessing(stopWords);
+		
+	}
+	
+	private static void drawTitle() {
+		
+		System.out.println("  _____ _____     _______     _______ _______ ______ __  __ ");
+		System.out.println(" |_   _|  __ \\   / ____\\ \\   / / ____|__   __|  ____|  \\/  |");
+		System.out.println("   | | | |__) | | (___  \\ \\_/ / (___    | |  | |__  | \\  / |");
+		System.out.println("   | | |  _  /   \\___ \\  \\   / \\___ \\   | |  |  __| | |\\/| |");
+		System.out.println("  _| |_| | \\ \\   ____) |  | |  ____) |  | |  | |____| |  | |");
+		System.out.println(" |_____|_|  \\_\\ |_____/   |_| |_____/   |_|  |______|_|  |_|");
+		
+	}
+	
+}
+
+
--- src/Preprocessing.java
+++ src/Preprocessing.java
@@ -1,4 +1,69 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
 
+/** 
+* @author Tejan Chinnakonda
+* 
+* @description
+* Functions for tokenization and stopword removal.
+* The index terms will be all the words left after filtering
+* punctuation tokens, numbers, stopwords, etc. 
+* 
+* @input/output
+* Inputs: Documents that are read on by one from the collection.
+* Outputs: Tokens to be added to the index (vocabulary).
+*/
 public class Preprocessing {
+	
+	private Scanner scanner;
+	
+	private String stopWordFile = "C:\\Users\\Tejan\\Dropbox\\workspace\\Microblog information retrieval system\\src\\StopWords.txt";
+	private ArrayList<String> stopWordList;
+	
+	private String documents; // input
+	
+	private String indexList; // output
+	
+	
+	public Preprocessing(String file) {
+		
+		this.documents = file;
+		
+		// Populate stop word list
+		stopWordList = new ArrayList<String>();
+		openFile(stopWordFile);
+		populateStopWordList();
+		closeFile();
+		
+		// Tokenize the documents that are read
+		
+		
+	}
+	
+	private void openFile(String file) {
+		try {
+			scanner = new Scanner(new File(file));
+		} 
+		catch (Exception e) {
+			System.out.println("Could not find file");
+		}
+	}
+	
+	private void populateStopWordList() {
+		
+		while (scanner.hasNext()) {
+			String str = scanner.next();
+			if (str != null)
+				stopWordList.add(str);
+		}
+		
+	}
+	
+	private void closeFile() {
+		scanner.close();
+	}
+		
+	
 	
 }
